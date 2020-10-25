23401dbd7ad199f8e83985462c918621da5b67c0
Logan S <schoyer.logan@outlook.be>
2020-10-25 13:59:54 +0100

    Mise en place d'un système de fichier pour enregistrer la position des joueurs

--- PluginTitle/src/be/sckoco/plugintitle/Main.java
+++ PluginTitle/src/be/sckoco/plugintitle/Main.java
@@ -1,10 +1,17 @@
 package be.sckoco.plugintitle;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitScheduler;
 
 import be.sckoco.plugintitle.commands.CommandBvn;
@@ -24,10 +31,41 @@ import be.sckoco.plugintitle.commands.CommandVille;
 
 public class Main extends JavaPlugin {
 	
-	@SuppressWarnings("deprecation")
+	private static final DateFormat formatFichier = new SimpleDateFormat("dd-MM-yyyy");
+	private static final DateFormat formatPos = new SimpleDateFormat("HH:mm:ss");
+	
+	private String date = formatFichier.format(new Date());
+	
 	@Override
 	public void onEnable() {
 		System.out.println("Lancement du plugin GamingRoom");
+		
+		File dir = new File("plugins/PluginGR");
+		if(!dir.exists()) {
+			if(dir.mkdir()) {
+				System.out.println("Dossier créé avec succès");
+			}
+			else {
+				System.out.println("Erreur à la création");
+			}
+		}
+		else {
+			System.out.println("Dossier déjà existant");
+		}
+		
+		File file = new File("plugins/PluginGR/" + date + ".txt");
+		if(!file.exists()) {
+			try {
+				file.createNewFile();
+				System.out.println("Fichier du "+ date + " créé");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		else {
+			System.out.println("Fichier du " + date + " déjà existant");
+		}
+		
 		getCommand("rdm").setExecutor(new CommandRdm());
 		getCommand("event").setExecutor(new CommandEvent());
 		getCommand("rdv").setExecutor(new CommandRdv());
@@ -47,11 +85,42 @@ public class Main extends JavaPlugin {
 		
 		BukkitScheduler scheduler = getServer().getScheduler();
 		scheduler.scheduleSyncRepeatingTask(this, new Runnable() {
+			
 			@Override
 			public void run() {
 				for(Player p : Bukkit.getWorld("world").getPlayers()) {
 					Location loc = p.getLocation();
 					System.out.println(p.getName() + " -> X : " + loc.getBlockX() + " Y : " + loc.getBlockY() + " Z : " + loc.getBlockZ());
+					try {
+						FileWriter writer = new FileWriter(file, true);
+						BufferedWriter bw = new BufferedWriter(writer);
+						String line = "[" + formatPos.format(new Date()) + " - " + p.getWorld().getName() + "] : " + p.getName() + " -> X : " + loc.getBlockX() + " Y : " + loc.getBlockY() + " Z : " + loc.getBlockZ();
+						bw.write(line + "\n");
+						bw.close();
+						writer.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				
+				}
+			}
+		}, 0, 900);
+		
+		scheduler.scheduleSyncRepeatingTask(this, new Runnable() {
+			
+			@Override
+			public void run() {
+				if(!date.equals(formatFichier.format(new Date()))) {
+					date = formatFichier.format(new Date());
+					File file = new File("plugins/PluginGR/" + date + ".txt");
+					if(!file.exists()) {
+						try {
+							file.createNewFile();
+							System.out.println("Fichier du "+ date + " créé");
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
 				}
 			}
 		}, 0, 900);
